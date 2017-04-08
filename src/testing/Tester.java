fec168d9c0cbad20358744d774b6b1ff1da73fbb
Tristram Newman <tssn2@cam.ac.uk>
2017-04-08 18:30:06 +0100

    Fixed build path issue

--- src/testing/Tester.java
+++ src/testing/Tester.java
@@ -5,7 +5,7 @@ import states.*;
 
 public class Tester {
 	public static void main(String[] args) {
-		GameWindow gw = new GameWindow("Yasha", 1024, 768, null);
+		GameWindow gw = new GameWindow("Yasha", 800, 600, null);
 		gw.setGameState(new DemoState(gw));
 		gw.startLoop();
 	}
